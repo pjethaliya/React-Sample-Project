8019de4f2cbbaed2dd944fe4173a4665b5cb57e9
pjethaliya <jethaliyapratik@gmail.com>
2020-06-10 14:23:04 -0400

    applying inline styles example

--- counter-app/src/Components/counter.jsx
+++ counter-app/src/Components/counter.jsx
@@ -3,14 +3,13 @@ import React, { Component } from "react";
 class Counter extends Component {
     state = {
         count:0,
-        imageUrl:'https://picsum.photos/200',
+        
     };
     render() {
         return (
-        <div>
-            <img src={this.state.imageUrl} alt=""></img>
-            <span>{this.formatCount()}</span>
-            <button>Increment</button>
+        <div>    
+            <span style={ { fontSize:30 } } className="badge badge-primary m-2">{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-sm">Increment</button>
         </div>
         );
     }
