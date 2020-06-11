07658937986cfedcfe50508b665d9465018056e3
pjethaliya <jethaliyapratik@gmail.com>
2020-06-10 20:26:49 -0400

    Implement delete method

--- counter-app/src/Components/counters.jsx
+++ counter-app/src/Components/counters.jsx
@@ -10,8 +10,10 @@ class Counters extends Component {
     ],
   };
 
-  handleDelete = () => {
-      console.log('Event handler called');
+  handleDelete = (counterId) => {
+      console.log('Event handler called',counterId );
+      const counters = this.state.counters.filter(c=>c.id !== counterId);
+      this.setState({ counters:counters });
   };
 
   render() {
@@ -20,8 +22,7 @@ class Counters extends Component {
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
-            value={counter.value}
-            id={counter.id}
+            counter={counter}
             onDelete={this.handleDelete}
           >
             <h4>Counter #{counter.id}</h4>
