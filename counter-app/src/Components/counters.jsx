0501478f4ac930df4227ab1cb83dc4eb82bd7973
pjethaliya <jethaliyapratik@gmail.com>
2020-06-10 20:01:38 -0400

    dynamically set the value using props

--- counter-app/src/Components/counters.jsx
+++ counter-app/src/Components/counters.jsx
@@ -3,16 +3,17 @@ import Counter from './counter';
 class Counters extends Component {
     state = {  
         counters : [
-            {id : 1, value : 0},
-            {id : 2, value : 0},
-            {id : 3, value : 0},
-            {id : 4, value : 0},
+            {id : 1, value : 4},
+            {id : 2, value : 3},
+            {id : 3, value : 2},
+            {id : 4, value : 1},
         ]
     };
     render() { 
         return ( 
             <div>
-                {this.state.counters.map(counter => <Counter key={counter.id} />)}
+                {this.state.counters.map(counter => <Counter 
+                key={counter.id} value={counter.value} />)}
             </div> 
         );
     }
