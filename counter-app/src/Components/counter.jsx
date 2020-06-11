534b8da4bf79e890fc8a7b9bd4392fc33594fff7
pjethaliya <jethaliyapratik@gmail.com>
2020-06-10 20:18:49 -0400

    deleting the counter using onDelete prop

--- counter-app/src/Components/counter.jsx
+++ counter-app/src/Components/counter.jsx
@@ -1,56 +1,63 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {
-        value:this.props.value,
-        tags : ['tag1','tag2','tag3'],
-        
-    };
+  state = {
+    value: this.props.value,
+    tags: ["tag1", "tag2", "tag3"],
+  };
 
-    //constructor(){
-    //    super();
-    //    this.handleIncrement = this.handleIncrement.bind(this);
-    //}
+  //constructor(){
+  //    super();
+  //    this.handleIncrement = this.handleIncrement.bind(this);
+  //}
 
-    handleIncrement = product => {
-        console.log(product);
-        this.setState({value:this.state.value + 1 });
-    };
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
+  };
 
-    render() {
-        return (
-        <div>  
-            {this.props.children}  
-            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button 
-                    onClick={()=>this.handleIncrement(10)} 
-                    className="btn btn-secondary btn-sm">
-                Increment
-            </button>
-            {this.renderTags()}
-        </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        {this.props.children}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement()}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={this.props.onDelete}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+        {this.renderTags()}
+      </div>
+    );
+  }
 
-    
-    renderTags(){
-        if(this.state.tags.length === 0) return <p>There are no tags</p>
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags</p>;
 
-        return <ul>
-                    {this.state.tags.map(tag=><li key={tag}>{tag}</li>)}
-                </ul>
-    }
-    getBadgeClasses(){
-        let classes = "badge m-2 badge-";
-        classes += this.state.value === 0 ? "warning" : "primary";
-        return classes; 
-    }
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
 
-    formatCount(){
-        const {value: count} = this.state;
-        return count === 0 ? 'Zero' :count;
-    }
-
+  formatCount() {
+    const { value: count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
 
 export default Counter;
--- counter-app/src/Components/counters.jsx
+++ counter-app/src/Components/counters.jsx
@@ -1,25 +1,35 @@
-import React, { Component } from 'react';
-import Counter from './counter';
+import React, { Component } from "react";
+import Counter from "./counter";
 class Counters extends Component {
-    state = {  
-        counters : [
-            {id : 1, value : 4},
-            {id : 2, value : 3},
-            {id : 3, value : 2},
-            {id : 4, value : 1},
-        ]
-    };
-    render() { 
-        return ( 
-            <div>
-                {this.state.counters.map(counter => <Counter 
-                key={counter.id} value={counter.value} >
-                <h4>Counter #{counter.id}</h4>
-                </Counter>
-                )}
-            </div> 
-        );
-    }
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 3 },
+      { id: 3, value: 2 },
+      { id: 4, value: 1 },
+    ],
+  };
+
+  handleDelete = () => {
+      console.log('Event handler called');
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            value={counter.value}
+            id={counter.id}
+            onDelete={this.handleDelete}
+          >
+            <h4>Counter #{counter.id}</h4>
+          </Counter>
+        ))}
+      </div>
+    );
+  }
 }
- 
+
 export default Counters;
